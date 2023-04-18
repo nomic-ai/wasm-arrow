64953a11b1cd3af07cf8b86456604997eb980bd4
Ben Schmidt <bmschmidt@gmail.com>
2023-04-18 00:24:16 -0400

    LGTM

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,36 @@
-use regex::Regex;
-use serde::{Deserialize, Serialize};
+use js_sys::{Float32Array};
+use regex::RegexBuilder;
+use std::str;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsValue;
-use serde_wasm_bindgen::{to_value, from_value};
-
-#[derive(Deserialize, Serialize)]
-pub struct StringData {
-    vector: Vec<u8>,
-    offsets: Vec<usize>,
-}
 
 #[wasm_bindgen]
-pub fn count_regex_matches(data: JsValue, regex: &str) -> Result<JsValue, JsValue> {
-    let string_data: StringData = from_value(data).map_err(|e| e.to_string())?;
-
-    let re = match Regex::new(regex) {
-        Ok(re) => re,
-        Err(e) => return Err(JsValue::from_str(&e.to_string())),
+pub fn count_regex_matches(vector: &[u8], offsets: &[usize], regex: &str, case_insensitive: bool) -> Float32Array {
+    let regex = match RegexBuilder::new(regex)
+        .case_insensitive(case_insensitive)
+        .build() {
+            Ok(r) => r,
+            Err(_) => {
+                let mut array = Float32Array::new_with_length(offsets.len() as u32 - 1);
+                for i in 0..(offsets.len() - 1) {
+                    array.set_index(i as u32, -1.0);
+                }
+                return array;
+            }
     };
-
-    let mut matches = Vec::new();
-    for i in 0..string_data.offsets.len() - 1 {
-        let start = string_data.offsets[i];
-        let end = string_data.offsets[i + 1];
-        let substring = String::from_utf8_lossy(&string_data.vector[start..end]);
-        let count = re.find_iter(&substring).count();
-        matches.push(count as f32);
+    
+    let mut matches = Vec::with_capacity(offsets.len() - 1);
+    for i in 0..(offsets.len() - 1) {
+        let start = offsets[i];
+        let end = offsets[i + 1];
+        let string_slice = &vector[start..end];
+        let count = match str::from_utf8(string_slice) {
+            Ok(valid_str) => regex.find_iter(valid_str).count() as f32,
+            Err(_) => -1.0,
+        };
+        matches.push(count);
     }
 
-    let result = to_value(&matches).map_err(|e| e.to_string())?;
-    Ok(result)
+    let array = Float32Array::new_with_length(matches.len() as u32);
+    array.copy_from(&matches);
+    array
 }
