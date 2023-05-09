26ffa6f8d551d95db36339e0a464262d6ab68449
Ben Schmidt <bmschmidt@gmail.com>
2023-05-09 09:30:29 -0400

    Add kv presence lookup for strings

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use js_sys::{Float32Array};
 use regex::RegexBuilder;
 use std::str;
 use wasm_bindgen::prelude::*;
+use std::collections::HashSet;
 
 #[wasm_bindgen]
 pub fn count_regex_matches(vector: &[u8], offsets: &[usize], regex: &str, case_insensitive: bool) -> Float32Array {
@@ -10,7 +11,7 @@ pub fn count_regex_matches(vector: &[u8], offsets: &[usize], regex: &str, case_i
         .build() {
             Ok(r) => r,
             Err(_) => {
-                let mut array = Float32Array::new_with_length(offsets.len() as u32 - 1);
+                let array = Float32Array::new_with_length(offsets.len() as u32 - 1);
                 for i in 0..(offsets.len() - 1) {
                     array.set_index(i as u32, -1.0);
                 }
@@ -34,3 +35,53 @@ pub fn count_regex_matches(vector: &[u8], offsets: &[usize], regex: &str, case_i
     array.copy_from(&matches);
     array
 }
+
+#[wasm_bindgen]
+pub struct ExactMatcher {
+    matchlist_strings: HashSet<String>,
+}
+
+#[wasm_bindgen]
+impl ExactMatcher {
+    #[wasm_bindgen(constructor)]
+    pub fn new(matchlist: &[u8], matchlist_offsets: &[usize]) -> ExactMatcher {
+        let matchlist_strings: HashSet<String> = matchlist_offsets
+            .windows(2)
+            .map(|offset_pair| {
+                let start = offset_pair[0];
+                let end = offset_pair[1];
+                str::from_utf8(&matchlist[start..end]).unwrap_or("").to_string()
+            })
+            .collect();
+
+        ExactMatcher { matchlist_strings }
+    }
+
+    #[wasm_bindgen]
+    pub fn find_exact_matches(&self, vector: &[u8], offsets: &[usize]) -> Float32Array {
+        let mut matches = Vec::with_capacity(offsets.len() - 1);
+
+        for i in 0..(offsets.len() - 1) {
+            let start = offsets[i];
+            let end = offsets[i + 1];
+            let string_slice = &vector[start..end];
+
+            let count = match str::from_utf8(string_slice) {
+                Ok(valid_str) => {
+                    if self.matchlist_strings.contains(valid_str) {
+                        1.0
+                    } else {
+                        0.0
+                    }
+                }
+                Err(_) => -1.0,
+            };
+            matches.push(count);
+        }
+
+        let array = Float32Array::new_with_length(matches.len() as u32);
+        array.copy_from(&matches);
+        array
+    }
+}
+
